cb30bd4db07a56ac1a5896381e2e8b2c1d1dfe39
kfaceapi <kfaceapi@gmail.com>
2020-10-11 08:56:03 +0900

    윈도우용 배치 파일 추가

---
+++ code/train.bat
@@ -0,0 +1,5 @@
+python train.py --stratified --fold 0
+python train.py --stratified --fold 1
+python train.py --stratified --fold 2
+python train.py --stratified --fold 3
+python train.py --stratified --fold 4
